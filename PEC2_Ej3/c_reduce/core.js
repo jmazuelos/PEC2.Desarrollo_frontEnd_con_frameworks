fdf488f951bda478a39c255a051722eb005cf47c
jmazuelos <jmazuelos@uoc.edu>
2023-11-11 23:46:03 +0100

    Borrador v3

--- PEC2_Ej3/c_reduce/core.js
+++ PEC2_Ej3/c_reduce/core.js
@@ -1,17 +1,18 @@
 function sum(array) {
-  // your code here
+  return array.reduce( (total, element) => total + element);
 }
 
 function productAll(array) {
-  // your code here
+  return array.map( matrixElement => matrixElement.reduce( (subtotal, subelement) => subtotal * subelement))
+              .reduce( (total, element) => total * element);
 }
 
 function objectify(array) {
-  // your code here
+  return array.reduce((object, element) => ({...object, [element[0]]: element[1]}), {});
 }
 
 function luckyNumbers(array) {
-  // your code here
+  return 'Your lucky numbers are: '.concat(array.reduce((string, element , index, arr) => index == arr.length-1 ? (string + ", and " + (element).toString()) : (string + ", " + (element).toString()) ))
 }
 
 module.exports = {
